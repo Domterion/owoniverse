f295701b5cfd95f35a0e5f12c9b69a069405ede0
dom <35433518+Domterion@users.noreply.github.com>
2020-05-17 21:55:33 -0400

    Added example config

---
+++ config.example.py
@@ -0,0 +1,19 @@
+token = "TOKEN"
+
+prefix = ["owo "]
+
+owner = "OWNER"
+color = 0x000000
+
+description = "A bot made by YOU."
+
+extensions = ["jishaku"]
+
+db = {
+    "host": "127.0.0.1",
+    "user": "postgres",
+    "database": "postgres",
+    "password": ""
+}
+
+emotes = {"tick": "✅", "cross": "❎"}
